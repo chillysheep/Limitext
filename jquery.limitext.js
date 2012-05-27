3653bc9665f322ab258bec354ad5cbed6ce3e592
James Baldwin <james@chillysheep.co.uk>
2012-05-27 16:58:18 +0100

    First commit
    jQuery plugin commit

---
+++ jquery.limitext.js
@@ -0,0 +1,31 @@
+/*!
+* jQuery Limitext Plugin
+* Examples at: http://limitext.jbaldwin.me
+* View project on GitHub at: 
+*/
+
+(function( $ ) {
+  $.fn.limitext = function(options) {
+    var defaults = {
+      output : $('.lt-output'),
+      limit : 250
+    }
+    var options = $.extend(defaults,  options);
+    return this.each(function() {
+       var charlimit = options.limit;
+       var output = options.output;
+       var _this = $(this);
+       
+       (_this.val() !== 0) ? output.html(charlimit - _this.val().length) : output.html(charlimit);
+
+       _this.keyup(function(){
+        if (charlimit < _this.val().length) {
+          _this.val(_this.val().substr(0, charlimit));
+        }
+        else {
+          output.html(charlimit - _this.val().length);   
+          }
+       });
+    });         
+  };
+})( jQuery ); 
