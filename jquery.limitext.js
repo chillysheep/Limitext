85cabc42ceb9510d8c6be4546059801f95f1f307
James Baldwin <james@chillysheep.co.uk>
2012-06-22 21:10:41 +0100

    Updated demo to include latest version of script

--- jquery.limitext.js
+++ jquery.limitext.js
@@ -4,4 +4,4 @@
 * View project on GitHub at: https://github.com/chillysheep/Limitext
 */
 
-(function(a){a.fn.limitext=function(b){var c={output:a(".lt-output"),limit:250};var b=a.extend(c,b);return this.each(function(){var c=b.limit;var d=b.output;var e=a(this);e.val()!==0?d.html(c-e.val().length):d.html(c);e.keyup(function(){if(c<e.val().length){e.val(e.val().substr(0,c))}else{d.html(c-e.val().length)}})})}})(jQuery)
+(function(a){a.fn.limitext=function(b){var c={output:a(".lt-output"),limit:250};var b=a.extend(c,b);return this.each(function(){var c=b.limit;var d=b.output;var e=a(this);e.val()!==0?d.html(c-e.val().length):d.html(c);e.on("input keyup propertychange",function(){if(c<e.val().length){e.val(e.val().substr(0,c))}else{d.html(c-e.val().length)}})})}})(jQuery)
